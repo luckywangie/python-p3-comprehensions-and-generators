5a632ae3c41beab41286d90dd0d1cf052a5ef892
lucky wangie <wangiemamati@gmail.com>
2025-02-13 12:39:32 +0300

    tests pass

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
-#!/usr/bin/env python3
+def return_evens(numbers):
+    return [n for n in numbers if n % 2 == 0]
 
-def return_evens(num_list):
-    pass
 
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
+
+
+# function 1 test
+return_evens([0, 1, 3, 5, 7, 8, 9])
+# use print
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
+#function 2 test
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+#use print 
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+)
